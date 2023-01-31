ffa8e96b504c5eb1504db158d6828f62814c8eff
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2023-01-31 18:56:04 +1000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,6 @@
-function Circle (R: number, I: number, X: number, Y: number) {
-    mySprite.setPosition(Math.sin(I) * R + X, Math.cos(I) * R + Y)
-}
-let projectile: Sprite = null
-let mySprite: Sprite = null
-mySprite = sprites.create(img`
-    . . . 
-    . . . 
-    . . . 
-    `, SpriteKind.Player)
-let a = 0
-let b = 10
-forever(function () {
-    a += 0.1
-    Circle(b, a, 80, 60)
-    pause(10)
-    projectile = sprites.createProjectileFromSprite(img`
-        2 
-        `, mySprite, 0, 0)
-})
-forever(function () {
-    for (let value of sprites.allOfKind(SpriteKind.Projectile)) {
-        sprites.destroy(value, effects.none, 3500)
+namespace Circle {
+    export function Circle (R: number, I: number, X: number, Y: number, Spr: Sprite) {
+    Spr.setPosition(Math.sin(I) * R + X, Math.cos(I) * R + Y)
     }
-})
+}
+
